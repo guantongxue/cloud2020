24b8ba47cac3583edf635c06d3f4c1d717a4e161
guannaiwei <939730442@qq.com>
2021-01-28 22:50:26 +0800

    config总控中心搭建

---
+++ cloud-config-center3344/src/main/java/com/minjiang/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.minjiang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @auther guannw
+ * @create 2021/1/28 22:04
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
